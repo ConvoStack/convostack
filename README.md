95232d2feabafe7da34d90ce864ea19d955321e8
Sasha Varlamov <sasha@sashavarlamov.com>
2023-05-18 17:41:05 -0400

    Add postgres and mysql to convostack/ package

--- README.md
+++ README.md
@@ -6,11 +6,14 @@ To learn more about the project and compatible technologies, check out [ConvoSta
 
 ## Getting started with ConvoStack
 
-To get started with ConvoStack, check out our getting started repo [here](https://github.com/ConvoStack/getting-started)!
+To get started with ConvoStack, check out our getting started
+repo [here](https://github.com/ConvoStack/getting-started)!
 
 ## Developing ConvoStack
 
-Only follow these instructions if you're looking to contribute to ConvoStack development. If you're looking to use ConvoStack in your own app, check out the getting started repo [here](https://github.com/ConvoStack/getting-started) instead.
+Only follow these instructions if you're looking to contribute to ConvoStack development. If you're looking to use
+ConvoStack in your own app, check out the getting started repo [here](https://github.com/ConvoStack/getting-started)
+instead.
 
 ### Monorepo setup
 
@@ -43,3 +46,30 @@ changeset version
 # Publish all changed packages to npm
 changeset publish
 ```
+
+### Add a monorepo package to the main convostack package
+
+All `@convostack/*` packages live in the `packages/` directory and follow the `convostack-package-name` folder naming
+convention. To add a new package, reference one of the many existing packages.
+
+Just because a package has been defined in the `packages/` directory does NOT mean that it will automatically available
+in the main `convostack` NPM package.
+
+In order to add a package to the main `convostack` package, you must:
+
+1. Add a new folder with the following naming convention to the `packages/convostack/src` folder. For example, for a
+   package named `@convostack/example-subpackage`, you would create the
+   directory `packages/convostack/src/example-subpackage` and a corresponding `index.ts`
+   file `packages/convostack/src/example-subpackage/index.ts` that would serve to re-export your original package from
+   within the parent `convostack` package. The `index.ts` file should only contain one
+   line: `export * from '@convostack/example-subpackage';`
+2. Add your package to the dependencies of `convostack`. Using the example from above, you would
+   add `"@convostack/example-subpackage": "*"` to `packages/convostack/package.json`'s `dependencies`. Please note that
+   for all of this to work for end users, you must publicly publish the `@convostack/example-subpackage` package to NPM,
+   since `convostack` does not actually bundle these dependencies internally.
+3. To ensure that the entrypoints for imports are properly generated, you must also add your package to
+   the `entrypoints` defined in the `packages/convostack/scripts/create-entrypoints.js` file. Using the example from
+   above,
+   you would add `"example-subpackage": "example-subpackage/index",` to the `entrypoints` map.
+4. When you run `turbo build` in the root of the monorepo, it will now automatically build and properly provide your
+   package via the main`convostack` package.
--- examples/be-example-express-sqlite/src/server-sse-demo.ts
+++ examples/be-example-express-sqlite/src/server-sse-demo.ts
@@ -1,13 +1,16 @@
-import { ConvoStackBackendExpress } from "convostack/backend-express";
+import {ConvoStackBackendExpress} from "convostack/backend-express";
 import express from "express";
 import {StorageEnginePrismaSQLite} from "convostack/storage-engine-prisma-sqlite";
-import cors, { CorsOptions } from "cors";
-import { AuthJWT } from "convostack/auth-jwt";
-import { createServer } from "http";
+import cors, {CorsOptions} from "cors";
+import {AuthJWT} from "convostack/auth-jwt";
+import {createServer} from "http";
 import * as dotenv from "dotenv";
-import { DefaultAgentManager } from "convostack/agent";
-import { AgentSSEClient } from "convostack/agent-sse";
-import { serveEchoAgentDev } from "convostack/agent-sse-echo-server";
+import {DefaultAgentManager} from "convostack/agent";
+import {AgentSSEClient} from "convostack/agent-sse";
+import {serveEchoAgentDev} from "convostack/agent-sse-echo-server";
+import {IStorageEngine} from "convostack/models";
+import {StorageEnginePrismaPostgres} from "convostack/storage-engine-prisma-postgres";
+import {StorageEnginePrismaMySQL} from "convostack/storage-engine-prisma-mysql";
 
 dotenv.config();
 
@@ -18,51 +21,66 @@ const sseHost = process.env.SSE_HOST || "localhost";
 console.log("Configuring server...");
 
 const corsOptions: CorsOptions = {
-  origin: ["http://localhost:5173", "https://studio.apollographql.com"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+    origin: ["http://localhost:5173", "https://studio.apollographql.com"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 };
 
 const main = async () => {
-  const app = express();
-  app.use(cors(corsOptions));
-  const httpServer = createServer(app);
-  const storage = new StorageEnginePrismaSQLite(process.env.DATABASE_URL);
-  await storage.init();
-  const backend = new ConvoStackBackendExpress({
-    basePath: "/",
-    storage,
-    auth: new AuthJWT(storage, {
-      jwtSecret: process.env.JWT_SECRET,
-      userDataVerificationSecret: process.env.USER_VERIFICATION_HASH_SECRET,
-      allowAnonUsers: process.env.ALLOW_ANONYMOUS_USERS == "true",
-      requireUserVerificationHash: !(
-        process.env.REQUIRE_USER_VERIFICATION_HASH == "false"
-      )
-    }),
-    agents: new DefaultAgentManager({
-      "default": {
-        agent: new AgentSSEClient(`http://${sseHost}:${ssePort}/api/chat`),
-        metadata: {
-          displayName: "Echo Agent",
-          primer: "This is demo echo agent. Write me a message, and I will send it back to you!"
-        }
-      }
-    }, "default")
-  });
+    const app = express();
+    app.use(cors(corsOptions));
+    const httpServer = createServer(app);
+    let storage: IStorageEngine;
+    switch (process.env.STORAGE_ENGINE) {
+        case 'sqlite':
+            storage = new StorageEnginePrismaSQLite(process.env.DATABASE_URL);
+            await (storage as StorageEnginePrismaSQLite).init();
+            break;
+        case 'postgres':
+            storage = new StorageEnginePrismaPostgres(process.env.DATABASE_URL);
+            await (storage as StorageEnginePrismaPostgres).init();
+            break;
+        case 'mysql':
+            storage = new StorageEnginePrismaMySQL(process.env.DATABASE_URL);
+            await (storage as StorageEnginePrismaMySQL).init();
+            break;
+        default:
+            throw new Error(`Invalid storage engine: ${process.env.STORAGE_ENGINE}`)
+    }
+    const backend = new ConvoStackBackendExpress({
+        basePath: "/",
+        storage,
+        auth: new AuthJWT(storage, {
+            jwtSecret: process.env.JWT_SECRET,
+            userDataVerificationSecret: process.env.USER_VERIFICATION_HASH_SECRET,
+            allowAnonUsers: process.env.ALLOW_ANONYMOUS_USERS == "true",
+            requireUserVerificationHash: !(
+                process.env.REQUIRE_USER_VERIFICATION_HASH == "false"
+            )
+        }),
+        agents: new DefaultAgentManager({
+            "default": {
+                agent: new AgentSSEClient(`http://${sseHost}:${ssePort}/api/chat`),
+                metadata: {
+                    displayName: "Echo Agent",
+                    primer: "This is demo echo agent. Write me a message, and I will send it back to you!"
+                }
+            }
+        }, "default")
+    });
 
-  await backend.init(app, httpServer);
+    await backend.init(app, httpServer);
 
-  console.log(`Starting server on port ${port}...`);
-  httpServer.listen(parseInt(port), host, () => {
-    console.log(`Server is running on http://${host}:${port}/graphql`);
-  });
+    console.log(`Starting server on port ${port}...`);
+    httpServer.listen(parseInt(port), host, () => {
+        console.log(`Server is running on http://${host}:${port}/graphql`);
+    });
 };
 
 try {
-  serveEchoAgentDev(parseInt(ssePort), sseHost, "/api/chat");
-  main();
+    serveEchoAgentDev(parseInt(ssePort), sseHost, "/api/chat");
+    main();
 } catch (err) {
-  console.error(err);
+    console.error(err);
 }
