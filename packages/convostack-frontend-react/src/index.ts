355d9f62b3dcd08c2ffef7df0e5dd760b2182bb4
radami <imanwiz1@gmail.com>
2023-05-30 15:45:11 -0400

    export fixes

--- packages/convostack-frontend-react/src/index.ts
+++ packages/convostack-frontend-react/src/index.ts
@@ -1,7 +1,7 @@
-export { default as ConvoStackWrapper } from "./ConvoStackWrapper"
-export { default as EmbedChat } from "./components/EmbeddableChat/ConvoStackEmbed"
-export { default as Message } from "./components/Message"
-export type { MessageProps } from "./components/Message"
-export type { ConvoStackWrapperProps } from "./ConvoStackWrapper"
+export { default as ConvoStackWrapper } from "./ConvoStackWrapper";
+export { default as EmbedChat } from "./components/EmbeddableChat/ConvoStackEmbed";
+export { default as Message } from "./components/Message";
+export type { MessageProps } from "./components/Message";
+export type { ConvoStackWrapperProps } from "./ConvoStackWrapper";
 export type { ConvoStackEmbedProps } from "./components/EmbeddableChat/ConvoStackEmbed";
-export { default as useConvoStack } from "./hooks/useConvoStack"
+export { default as useConvoStack } from "./hooks/useConvoStack";
