1f90adeb3352211fbd6b7ba049e883796c922609
radami <imanwiz1@gmail.com>
2023-05-31 15:37:56 -0400

    test loading again

--- docs/src/theme/Layout/index.js
+++ docs/src/theme/Layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '@theme-original/Layout';
 import Loadable from "@loadable/component"
+import { ConvoStackEmbedProps } from 'convostack/frontend-react';
 
 export default function LayoutWrapper(props) {
   const ConvoStackDynamic = Loadable(() => import("../../ConvoStackDynamic"));
