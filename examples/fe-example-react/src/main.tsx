2fc623518a486eb0943272c11f5b04f377bce44d
Sasha Varlamov <sasha@sashavarlamov.com>
2023-06-07 13:54:07 -0400

    Add packages for one-line hosted playground

--- examples/fe-example-react/src/main.tsx
+++ examples/fe-example-react/src/main.tsx
@@ -13,15 +13,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <ConvoStackWrapper>
       <ConvoStackWidget
-        graphqlUrl="http://localhost:3033/graphql"
-        websocketUrl="ws://localhost:3033/graphql"
-        workspaceId="clijh54bz0009wud1v4yhrt8o"
-        userData={{
-          email: "m@sdfgfsdgg.com",
-          name: "zxx",
-          hash: "z420",
-          userId: "mmsxsddfmm",
-        }}
+        graphqlUrl="http://localhost:3000/graphql"
+        websocketUrl="ws://localhost:3000/graphql"
+        // workspaceId="clijh54bz0009wud1v4yhrt8o"
+        // userData={{
+        //   email: "m@sdfgfsdgg.com",
+        //   name: "zxx",
+        //   hash: "z420",
+        //   userId: "mmsxsddfmm",
+        // }}
         customStyling={{
           headerText: "My Custom Header",
           widgetLocation: "right",
---
+++ packages/convostack/src/langchain-agent/index.ts
@@ -0,0 +1,1 @@
+export * from '@convostack/langchain-agent';
